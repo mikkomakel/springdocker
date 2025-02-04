69a13b7d74d5d91ace626a4f4d26956a41fee62c
mikkomakel <mikko.makela@seamk.fi>
2025-02-04 12:52:08 +0200

    bhklbhh

--- src/main/java/com/example/springdocker/SpringdockerApplication.java
+++ src/main/java/com/example/springdocker/SpringdockerApplication.java
@@ -6,9 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringdockerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringdockerApplication.class, args);
-		//niijjuu 
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SpringdockerApplication.class, args);
+        //niijjuu
+    }
 }
