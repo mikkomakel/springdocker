0b0339efe365ed1811c5293caeb838b6e0e899a7
mikkomakel <mikko.makela@seamk.fi>
2025-02-11 10:25:56 +0200

    asd

--- src/main/java/com/example/springdocker/SpringdockerApplication.java
+++ src/main/java/com/example/springdocker/SpringdockerApplication.java
@@ -9,5 +9,6 @@ public class SpringdockerApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringdockerApplication.class, args);
         //niijjuu
+        //juuuu
     }
 }
